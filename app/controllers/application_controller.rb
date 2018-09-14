44e609fb86ca0dc0c67434dd5741083506b9cfb6
shimahikaru <i12007ushio@gmail.com>
2018-09-14 15:35:05 +0900

    ユーザー登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys:[:familyname, :firstname, :office])
+      devise_parameter_sanitizer.permit(:sign_in, keys:[:familyname, :firstname, :office])
+      devise_parameter_sanitizer.permit(:account_update, keys:[:familyname, :firstname, :office])
+    end
+
 end
