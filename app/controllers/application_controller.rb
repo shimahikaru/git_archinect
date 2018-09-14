8aa02b11f63428e67eab04b95b9b21edf7d63b29
shimahikaru <i12007ushio@gmail.com>
2018-09-14 19:30:43 +0900

    ユーザーログイン完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
 before_action :configure_permitted_parameters, if: :devise_controller?
+before_action :authenticate_user!, except: [:index]
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys:[:familyname, :firstname, :office])
-      devise_parameter_sanitizer.permit(:sign_in, keys:[:familyname, :firstname, :office])
-      devise_parameter_sanitizer.permit(:account_update, keys:[:familyname, :firstname, :office])
+      devise_parameter_sanitizer.permit(:sign_up, keys:[:familyname, :firstname, :office, :location])
+      devise_parameter_sanitizer.permit(:sign_in, keys:[:familyname, :firstname, :office, :location])
+      devise_parameter_sanitizer.permit(:account_update, keys:[:familyname, :firstname, :office, :location])
     end
+
+    def after_sign_in_path_for(resource)
+       user_path(current_user)
+    end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_13_043627) do
+ActiveRecord::Schema.define(version: 2018_09_14_083301) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2018_09_13_043627) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "location", null: false
+    t.string "encrypted_password", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
