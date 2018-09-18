f48af193177f3585a96bee2b8abafa8684a70c3e
shimahikaru <i12007ushio@gmail.com>
2018-09-18 19:23:37 +0900

    product投稿ページ作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,9 +9,9 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @product = Product.new
-    if @product.save(creatte_params)
-    redirect_to controller: 'products', action: 'show'
+     @user = User.find(current_user.id)
+    if Product.create(create_params)
+    redirect_to user_product_path(@user, @product)
     else
     render :new
     end
@@ -21,7 +21,7 @@ class ProductsController < ApplicationController
   private
 
   def create_params
-    params.require(:product).permit(:title, :whet, :completion, :location, :location2, :area, :text, whole_photos_attributes:[:photo], details_attributes:[:image, :title, :text], genre_tag_attributes:[:genre1, :genre2, :genre3, :genre4, :genre5, :genre6, :genre7, :genre8, :genre9, :genre10, :genre11])
+    params.require(:product).permit(:title, :whet, :completion, :location, :location2, :area, :text, whole_photos_attributes:[:photo], details_attributes:[:image, :title, :text], genre_tag_attributes:[:genre1, :genre2, :genre3, :genre4, :genre5, :genre6, :genre7, :genre8, :genre9, :genre10, :genre11]).merge(user_id: current_user.id)
   end
 
 end
