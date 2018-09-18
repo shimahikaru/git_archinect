364fd4b3961ca09e564af971f871d43ce72b70e4
shimahikaru <i12007ushio@gmail.com>
2018-09-18 15:51:02 +0900

    product投稿ページ編集

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,9 +3,25 @@ class ProductsController < ApplicationController
   def new
   @user = User.find(current_user.id)
   @product = Product.new
+  @product.build_genre_tag
+  @product.details.build
+  4.times { @product.whole_photos.build }
   end
 
   def create
+    @product = Product.new
+    if @product.save(creatte_params)
+    redirect_to controller: 'products', action: 'show'
+    else
+    render :new
+    end
+  end
+
+
+  private
+
+  def create_params
+    params.require(:product).permit(:title, :whet, :completion, :location, :location2, :area, :text, whole_photos_attributes:[:photo], details_attributes:[:image, :title, :text], genre_tag_attributes:[:genre1, :genre2, :genre3, :genre4, :genre5, :genre6, :genre7, :genre8, :genre9, :genre10, :genre11])
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,13 @@
 class Product < ApplicationRecord
+
   belongs_to :user
+
   has_many :whole_photos
   accepts_nested_attributes_for :whole_photos, allow_destroy: true
+
   has_many :details
   accepts_nested_attributes_for :details, allow_destroy: true
+
   has_one :genre_tag
   accepts_nested_attributes_for :genre_tag, allow_destroy: true
 
