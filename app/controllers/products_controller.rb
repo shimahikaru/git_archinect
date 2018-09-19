a0a8beb593709579d6691c3246c51eb1755cc0ef
shimahikaru <i12007ushio@gmail.com>
2018-09-19 12:45:07 +0900

    投稿ページ編集

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,25 +3,27 @@ class ProductsController < ApplicationController
   def new
   @user = User.find(current_user.id)
   @product = Product.new
-  @product.build_genre_tag
-  @product.details.build
-  4.times { @product.whole_photos.build }
+  # @product.build_genre_tag
+  # @product.details.build
+  # 4.times { @product.whole_photos.build }
   end
 
   def create
-     @user = User.find(current_user.id)
-    if Product.create(create_params)
-    redirect_to user_product_path(@user, @product)
-    else
-    render :new
-    end
+     # @product = Product.new
+     @product = Product.create(create_params)
+    # if @product.save(create_params)
+    # redirect_to controller: 'users', action: 'show', id: 2
+    # else
+    # render :new
+    # end
   end
 
 
   private
 
   def create_params
-    params.require(:product).permit(:title, :whet, :completion, :location, :location2, :area, :text, whole_photos_attributes:[:photo], details_attributes:[:image, :title, :text], genre_tag_attributes:[:genre1, :genre2, :genre3, :genre4, :genre5, :genre6, :genre7, :genre8, :genre9, :genre10, :genre11]).merge(user_id: current_user.id)
+    params.require(:product).permit(:title, :subtitle, :whet, :completion, :location, :location2, :area, :text).merge(user_id: current_user.id)
+    # params.require(:product).permit(:title, :whet, :completion, :location, :location2, :area, :text, whole_photos_attributes:[:photo], details_attributes:[:image, :title, :text], genre_tag_attributes:[:genre1, :genre2, :genre3, :genre4, :genre5, :genre6, :genre7, :genre8, :genre9, :genre10, :genre11]).merge(user_id: current_user.id)
   end
 
 end
