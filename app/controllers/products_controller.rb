e213ebcbef6a9dfd9bd71fe158add709b866bf13
shimahikaru <i12007ushio@gmail.com>
2018-09-28 20:36:54 +0900

    product edit完成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,18 +22,13 @@ class ProductsController < ApplicationController
   def new
   @product = Product.new
   @user = User.find(current_user.id)
-   @product.details.build
-   @product.whole_photos.build
+  @product.details.build
+  @product.whole_photos.build
   @product.user = @user
   end
 
   def create
      @product = Product.new(create_params)
-     # details = Detail.new(create_detail_params)
-     # whole_photos = WholePhoto.new(create_whole_photo_params)
-
-     # @product.details << details
-
 
     if @product.save
     redirect_to user_path(current_user)
@@ -43,6 +38,21 @@ class ProductsController < ApplicationController
   end
 
   def edit
+  @product = Product.find(params[:id])
+   @user = current_user
+     # @product.whole_photos.photo.cache! unless @product.whole_photos.photo.blank?
+     #   @product.details.image.cache! unless @product.details.image.blank?
+  end
+
+  def update
+    @product = Product.find(params[:id])
+   if user_signed_in? && current_user == @product.user
+     if @product.update(update_params)
+     redirect_to user_path(current_user)
+     else
+     render action: :edit
+     end
+   end
   end
 
   private
@@ -50,6 +60,12 @@ class ProductsController < ApplicationController
   def create_params
     params.require(:product).permit(:title, :subtitle, :whet, :completion, :location, :area, :text, :category, { :genre_ids=> [] }, details_attributes:[:id, :image, :title, :text, :image_cache, :_destroy ], whole_photos_attributes:[:id, :photo, :photo_cache, :_destroy] ).merge(user_id: current_user.id)
   end
+
+  def update_params
+    params.require(:product).permit(:title, :subtitle, :whet, :completion, :location, :area, :text, :category, { :genre_ids=> [] }, details_attributes:[:id, :image, :title, :text, :image_cache, :_destroy ], whole_photos_attributes:[:id, :photo, :photo_cache, :_destroy] ).merge(user_id: current_user.id)
+  end
+
+
 
   # def create_detail_params
 
