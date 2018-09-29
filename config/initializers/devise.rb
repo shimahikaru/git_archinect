c16298fafd2271ed4485bb21cff9dcd162d402de
shimahikaru <i12007ushio@gmail.com>
2018-09-29 15:28:51 +0900

    viewer作成開始

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = 'f2adabd1bf8d4184dd3c73cddd29526decebedc8e3f81137f662ecc6cfd6d13b10c24628ab7712aa4006ff13f46d52e0443500bf8e8794caef96f8e302480100'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -229,7 +229,7 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+  config.scoped_views = false
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  root 'top#index'
+
+  devise_for :viewers, controllers: {
+  sessions:      'viewers/sessions',
+  passwords:     'viewers/passwords',
+  registrations: 'viewers/registrations'
+}
+
 
   devise_for :users
-  root 'top#index'
+
 
   resources :users, only: [:index, :show, :edit, :update] do
     collection do
