a94e3b6ee0c21e476b29d449fa51d3a8eb5a701d
shimahikaru <i12007ushio@gmail.com>
2018-09-29 17:08:46 +0900

    viewer削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
   root 'top#index'
 
-  devise_for :viewers, controllers: {
-  sessions:      'viewers/sessions',
-  passwords:     'viewers/passwords',
-  registrations: 'viewers/registrations'
-}
+#   devise_for :viewers, controllers: {
+#   sessions:      'viewers/sessions',
+#   passwords:     'viewers/passwords',
+#   registrations: 'viewers/registrations'
+# }
 
+#   resources :viewers, only: [:index, :show, :edit, :update] do
+#     collection do
+#       get :search
+#     end
+#   end
 
   devise_for :users
-
 
   resources :users, only: [:index, :show, :edit, :update] do
     collection do
