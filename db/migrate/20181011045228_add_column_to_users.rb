67d4f8277ce12629b2bf2b17a00e06952512d113
shimahikaru <i12007ushio@gmail.com>
2018-10-18 19:05:57 +0900

    editview

--- db/migrate/20181011045228_add_column_to_users.rb
+++ db/migrate/20181011045228_add_column_to_users.rb
@@ -1,5 +1,8 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :followers_count, :integer, null: false, default: 0
+    add_column :users, :address, :string
+    add_column :users, :local, :string
+    add_column :users, :phone, :integer
   end
 end
