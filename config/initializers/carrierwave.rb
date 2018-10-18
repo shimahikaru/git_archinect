c330f5fb88b535093ba9b311b76d48594bba2a73
shimahikaru <i12007ushio@gmail.com>
2018-10-18 11:09:39 +0900

    add  acsesskey

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,22 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+    region: 'ap-northeast-1'
+  }
+
+    case Rails.env
+    when 'development'
+        config.fog_directory  = 'architect.shima'
+        config.asset_host = 'https://s3.amazonaws.com/architect.shima'
+    when 'production'
+        config.fog_directory  = 'architect.shima'
+        config.asset_host = 'https://s3.amazonaws.com/architect.shima'
+    end
+end
