1af3dd58035fd98443797c44eb2eec9d7aa2cdff
shimahikaru <i12007ushio@gmail.com>
2018-10-20 15:35:30 +0900

    add migration

--- db/migrate/20181020062551_remove_column_to_users.rb
+++
@@ -1,8 +0,0 @@
-class RemoveColumnToUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :followers_count, :integer, null: false, default: 0
-    remove_column :users, :address, :string
-    remove_column :users, :local, :string
-    remove_column :users, :phone, :integer
-  end
-end
