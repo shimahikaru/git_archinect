48e7cae5995ae6c9bf864cb0e3375d48ed39b891
shimahikaru <i12007ushio@gmail.com>
2018-10-20 16:21:11 +0900

    add S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
     region: 'ap-northeast-1'
   }
 
