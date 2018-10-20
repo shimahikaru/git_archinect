c6af0536033225d57b38eb7fe9f200bba67ae31b
shimahikaru <i12007ushio@gmail.com>
2018-10-20 15:30:58 +0900

    add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_17_024614) do
+ActiveRecord::Schema.define(version: 2018_10_20_062832) do
 
   create_table "Products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -131,9 +131,6 @@ ActiveRecord::Schema.define(version: 2018_10_17_024614) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "location", null: false
-    t.string "phone"
-    t.string "address"
-    t.string "local"
     t.string "encrypted_password", default: "", null: false
     t.string "work"
     t.boolean "offer"
@@ -141,6 +138,10 @@ ActiveRecord::Schema.define(version: 2018_10_17_024614) do
     t.string "avatar"
     t.string "text"
     t.integer "products_count", default: 0, null: false
+    t.integer "followers_count", default: 0, null: false
+    t.string "address"
+    t.string "local"
+    t.integer "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
