0689500ad43b55743c1eb0f815f68b1f7be419c8
shimahikaru <i12007ushio@gmail.com>
2018-10-22 21:14:11 +0900

    add view

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,6 +2,16 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
+module MiniMagick
+    def fix_exif_rotation
+      manipulate! do |img|
+        img.auto_orient
+        img = yield(img) if block_given?
+        img
+      end
+    end
+  end
+
 CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_credentials = {
