6522fd4f035072fab1e7c8dc6ff699014c425ace
shimahikaru <i12007ushio@gmail.com>
2018-10-25 19:56:11 +0900

    add view

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -7,12 +7,11 @@ class InquiryMailer < ApplicationMailer
   #
   # def inquiry_email
   #   @greeting = "Hi"
-
   #   mail to: "to@example.org"
   # end
   def inquiry_email(inquiry)
     @inquiry = inquiry
-    mail(to: 'to@example.com', subject: '新規お問い合わせがあります')
+    mail(to:  ENV['SEND_TO_MAIL'], subject: '新規お問い合わせがあります')
   end
 
 end
