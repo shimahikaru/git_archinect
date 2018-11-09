354ac45556a12abbe13175483b3608fd2daa400f
shimahikaru <i12007ushio@gmail.com>
2018-11-09 15:18:15 +0900

    edit css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 
-
-//= require swiper
 //= require turbolinks
+//= require swiper
 //= require jquery
 //= require jquery_ujs
 //= require cocoon
