b4ef0ce273c7403c7c52b8045c588cf44a8a2c26
shimahikaru <i12007ushio@gmail.com>
2018-11-09 21:13:44 +0900

    edit css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 
-
-//= require turbolinks
 //= require jquery
+//= require turbolinks
 //= require jquery_ujs
 //= require jquery-ui
 //= require activestorage
